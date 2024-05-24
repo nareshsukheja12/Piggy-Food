5e13fadd37e10bec4ac08c8db8c1d2e655311ed4
nareshsukheja12 <nareshsukheja12@gmail.com>
2024-05-24 10:38:46 +0530

    chore : fix typo in useRestaurantMenu import

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,6 +1,6 @@
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import useRestaurantMenu from "../utils/useRestrauntMenu";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 import RestaurantCategory from "./RestaurantCategory";
 import { useState } from "react";
 
